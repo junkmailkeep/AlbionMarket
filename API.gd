65ce0680154f5715e6e5c471faca2939f0fb7702
junkmailkeep <57474566+junkmailkeep@users.noreply.github.com>
2023-08-18 19:37:06 -0400

    Started API Project
    Need to create URLS

---
+++ API.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+
+
+func _on_cloth_button_pressed():
+	$HTTP_Cloth.request(URL.Cloth)
+	
+
+
+func _on_leather_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_plate_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_http_cloth_request_completed(result, response_code, headers, body):
+	var json = JSON.new()
+	json.parse(body)
+	var response = json.get_data()
